25e4c0bc029e08fde4db4d47d090540f2793034a
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2019-06-27 16:16:37 +0200

    add dependencies checks

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
+import shutil
+import sys
+
 import setuptools
 
 if __name__ == '__main__':
+    executable = shutil.which('pass')
+    if not executable:
+        sys.exit('`pass` executable not found util not found in your PATH!')
+    print('`pass` found at {}'.format(executable))
+
     setuptools.setup(
         name='keyring_password_store',
+        python_requires='>3.3',
         version='0.1.0',
         packages=['keyring_password_store'],
         entry_points={
             'keyring.backends': [
                 'pass = keyring_password_store'
             ]
-        }
+        },
+        install_requires=[
+            'keyring'
+        ]
     )
